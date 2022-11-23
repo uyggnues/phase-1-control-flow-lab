04614fc1ba39d70664bc5981a50089cdc68b8edf
Seunggyu <seunggyul14@icloud.com>
2022-11-23 09:02:23 -0800

    commit tree 187f90239846a46096c4b54950ae36207b9f6b79

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length) {
+  let result = '';
+  if (length <= 400) {
+    result = 'This one is on me!';
+  } else if (length > 400 && length <= 2000) {
+    result = 'That will be twenty bucks.';
+  } else if (length > 2000 && length <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (length > 2500) {
+    result = 'No can do.';
+  }
+ return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous' : return 'Thank you so much.'
+    case 'not as generous' : return 'Thank you.'
+      break;
+      default : return 'Bye.'
+  }
 }
